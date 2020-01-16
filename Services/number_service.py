97cd1613904efe31465ec14dec104759e1f07f50
Otto <otto@masterbranch.io>
2020-01-16 13:21:10 +0000

    functionality ready

---
+++ Services/number_service.py
@@ -0,0 +1,3 @@
+def get_list():
+    numbers_to_add = list(range(10000001))
+    return numbers_to_add
--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-def method():
-    return 'Hello World!'
+from flask import Flask, jsonify
 
+from Services import number_service
+from config import PORT_NUMBER
+
+app = Flask(__name__)
+
+
+@app.route('/total')
+def get_total():
+    lst = number_service.get_list()
+    result = {'total': sum(lst)}
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.config.from_object('config')
+    app.run(port=PORT_NUMBER)
